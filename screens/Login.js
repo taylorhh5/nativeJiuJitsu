5ca9bedd57416edc4148a641975d84aa52dd3d39
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-11-13 12:58:12 -0500

    Async Storage working

--- screens/Login.js
+++ screens/Login.js
@@ -1,22 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 export default function Login() {
+  const [name, setName] = useState();
 
+  const storeData = async () => {
+    try {
+      await AsyncStorage.setItem("MyName", name);
+    } catch (error) {
+      alert(error);
+    }
+  };
+  const getData = async () => {
+    try {
+      const name = await AsyncStorage.getItem("MyName");
+      if (name !== null) {
+        setName(name);
+      }
+    } catch (error) {
+      // error reading value
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const credentials = {
     email: "",
     password: "",
   };
-//   const [signUp, setSignUp] = useState({
-//     email: "",
-//     password: "",
-//   });
+  //   const [signUp, setSignUp] = useState({
+  //     email: "",
+  //     password: "",
+  //   });
 
-const [signUp, setSignUp] = useState(credentials);
-
-
+  const [signUp, setSignUp] = useState(credentials);
 
   //When typing in email input detected, setSignup to everything in signup but update email with value
   const handleEmailChange = (val) => {
@@ -33,10 +54,8 @@ const [signUp, setSignUp] = useState(credentials);
       password: val,
     });
   };
-
-
+  AsyncStorage.getItem("MyName").then((res) => console.log(res, "name"));
   const handleSubmit = () => {
-
     axios
       .post("https://jiujitsux.herokuapp.com/api/users/login", signUp)
       .then((response) => {
@@ -45,12 +64,18 @@ const [signUp, setSignUp] = useState(credentials);
       .catch((err) => console.log("register error"));
   };
 
-
   return (
     <View>
+      <Button title="Save name" onPress={() => storeData()} />
+      <Text>Name is: {name}</Text>
       <Text>{signUp.email}</Text>
       <Text>{signUp.password}</Text>
       <Text style={styles.registerHeader}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        onChangeText={(text) => setName(text)}
+      ></TextInput>
       <TextInput
         style={styles.input}
         placeholder="Email"
